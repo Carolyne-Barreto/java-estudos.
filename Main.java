6b9c59cae26a6becc07d08c45f69064ed1ef52d2
Carolyne Barreto <carolynedebarreto@gmail.com>
2026-09-23 20:07:32 -0300

    Implement main banking application logic

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 0;
+        double saldo = 0;
+        double deposito = 0;
+        double transferir = 0;
+        String resposta = "";
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Bem vindo, digite seu nome para começar.");
+        String nome = leitura.nextLine();
+
+        while (numero != 4) {
+
+            while (numero < 1 || numero > 4) {
+                System.out.println("***********************\n" +
+                        "Bem-vindo(a) " + nome + ".\n" +
+                        "Digite a opção desejada para\n" +
+                        "1- Consultar saldos\n" +
+                        "2- Depositar valor\n" +
+                        "3- Transferir valor\n" +
+                        "4- Sair\n");
+                numero = leitura.nextInt();
+            }
+            if (numero == 1) {
+                System.out.println("***********************\n" +
+                        "Nome:" + nome + "\n" +
+                        "Tipo conta: Corrente\n" +
+                        "Saldo inicial:" + saldo + "\n" +
+                        "***********************\n" +
+                        "\n" +
+                        " Confirme 0 para voltar ao Inicio.\n");
+                numero = leitura.nextInt();
+
+            } else if (numero == 2) {
+                while ( !resposta.equalsIgnoreCase("sim")) {
+                    System.out.println("Digite o valor que irá depositar:");
+                    deposito = leitura.nextDouble();
+                    System.out.println("Digite SIM para confirmar o valor ou 0 para retornar: R$" + deposito);
+                    resposta = leitura.next();
+
+                }
+            }
+
+
+        }
+
+
+    }
+}
+
